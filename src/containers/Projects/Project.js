b05200f271e404d661a2822387f73fe0bb64e52d
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-07-20 23:31:18 -0400

    add project photos and add them to the project page

--- src/containers/Projects/Project.js
+++ src/containers/Projects/Project.js
@@ -12,88 +12,146 @@ class Project extends Component {
         <NavigationBar/>
         <Title>My Projects, see what I built</Title>
 
-// now can set margin bottom, since it inheritated from Div
-      	<Relative marginBottom="40px" marginTop="125px">
+
+      	<Relative marginBottom="60px" marginTop="125px">
       		<Index>
       			 <h1>01</h1>
       		</Index>
             <h1> the project One <A href="#" target="_blank">name</A></h1>
       	</Relative>
 
-     <Flex justify={ 'center'  } marginBottom="100px">
+         <Flex justify={ 'center'  } marginBottom="200px">
+            <ReactZoomy
+    		    imageUrl={require('../../assets/news-republic520.png')}
+    		    renderThumbnail={({ showImage }) =>
+    		    <ImageButton onClick={showImage}>
+    		  	<img src = {require('../../assets/news-republic520.png')}
+    		     alt="project image"/>
+    		  </ImageButton>
+              }
+    		  scale={[1.1, 1.1]}
+    		  imageProps={{
+    		    style: {
+    		      width: '100vw',
+    		      height: 'auto'
+    		    }
+    		  }}
+            />
+          </Flex>
 
-        <ReactZoomy
-		    imageUrl={require('../../assets/k.jpg')}
-		    renderThumbnail={({ showImage }) =>
-		  <ImageButton onClick={showImage}>
-		  	<img src = {require('../../assets/k-thumbnail.jpg')}
-		     alt="project image"/>
-		  </ImageButton>
-          }
-
-		  scale={[1.1, 1.1]}
-		  imageProps={{
-		    style: {
-		      width: '100vw',
-		      height: 'auto'
-		    }
-		  }}
-        />
-     </Flex>
-
-     <Relative marginBottom="40px">
+     <Relative marginBottom="60px">
        <Index>
           <h1>02</h1>
        </Index>
          <h1> the project Two <A href="#" target="_blank">name</A></h1>
      </Relative>
 
-     <ImageContainer>
-         <ParallaxImage
-            reduceHeight={1/3}
-            src={require('../../assets/cat.jpg')}
-         />
-     </ImageContainer>
+    <Flex justify={ 'center' } marginBottom="200px" >
 
-    //  just to add space to show the scroll effect
-     <br/>
-      <br/>
-       <br/>
-        <br/>
-         <br/>
-          <br/>
+       <ReactZoomy
+       imageUrl={require('../../assets/responsive-website.png')}
+       renderThumbnail={({ showImage }) =>
+       <ImageButton onClick={showImage}>
+         <img src = {require('../../assets/responsive-website.png')}
+          alt="project image"/>
+       </ImageButton>
+           }
 
-          <Relative marginBottom="40px">
+         scale={[1.1, 1.1]}
+         imageProps={{
+           style: {
+             width: '100vw',
+             height: 'auto'
+           }
+         }}
+        />
+    </Flex>
+
+
+          <Relative marginBottom="60px">
         		<Index>
         			 <h1>03</h1>
         		</Index>
               <h1> the project Three <A href="#" target="_blank">name</A></h1>
         	</Relative>
 
-       <Flex justify={ 'center' } >
+           <Flex justify={ 'center' } marginBottom="200px">
 
-          <ReactZoomy
-  		    imageUrl={require('../../assets/k.jpg')}
-  		    renderThumbnail={({ showImage }) =>
-  		  <ImageButton onClick={showImage}>
-  		  	<img src = {require('../../assets/k-thumbnail.jpg')}
-  		     alt="project image"/>
-  		  </ImageButton>
-            }
+              <ReactZoomy
+      		    imageUrl={require('../../assets/v-hab.png')}
+      		    renderThumbnail={({ showImage }) =>
+          		  <ImageButton onClick={showImage}>
+          		  	<img src = {require('../../assets/v-hab.png')}
+          		     alt="project image"/>
+          		  </ImageButton>
+                    }
 
-  		  scale={[1.1, 1.1]}
-  		  imageProps={{
-  		    style: {
-  		      width: '100vw',
-  		      height: 'auto'
-  		    }
-  		  }}
-          />
+          		  scale={[1.1, 1.1]}
+          		  imageProps={{
+          		    style: {
+          		      width: '100vw',
+          		      height: 'auto'
+          		    }
+          		  }}
+                  />
+           </Flex>
+
+       <Relative marginBottom="60px">
+         <Index>
+            <h1>04</h1>
+         </Index>
+           <h1> the project Three <A href="#" target="_blank">name</A></h1>
+       </Relative>
+
+       <Flex justify={ 'center' } marginBottom="200px">
+           <ReactZoomy
+           imageUrl={require('../../assets/data.png')}
+           renderThumbnail={({ showImage }) =>
+           <ImageButton onClick={showImage}>
+           <img src = {require('../../assets/data.png')}
+            alt="project image"/>
+           </ImageButton>
+             }
+
+           scale={[1.1, 1.1]}
+           imageProps={{
+           style: {
+             width: '100vw',
+             height: 'auto'
+           }
+           }}
+           />
        </Flex>
 
+       <Relative marginBottom="60px">
+         <Index>
+            <h1>05</h1>
+         </Index>
+           <h1> the project Three <A href="#" target="_blank">name</A></h1>
+       </Relative>
 
+       <Flex justify={ 'center' } marginBottom="200px">
 
+       <ReactZoomy
+       imageUrl={require('../../assets/js.png')}
+       renderThumbnail={({ showImage }) =>
+       <ImageButton onClick={showImage}>
+       <img src = {require('../../assets/js.png')}
+        alt="project image"/>
+       </ImageButton>
+         }
 
+       scale={[1.1, 1.1]}
+       imageProps={{
+       style: {
+         width: '100vw',
+         height: 'auto'
+       }
+       }}
+       />
+       </Flex>
+
+       <A href="https://github.com/jessieyang0320" target="_blank">View More on Github</A>
 
       </Container>
     );
