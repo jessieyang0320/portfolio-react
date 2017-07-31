05b7fdbe7ba38e27784ef98b83b5b56d21e5137d
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-07-31 13:02:24 -0400

    created a image carousel component called ImageSlick

--- src/containers/AboutMe/Facts/entreprenuer.js
+++ src/containers/AboutMe/Facts/entreprenuer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ImageSlick from '../../../components/ImageSlick/ImageSlick'
+
 
 class Entreprenuer extends Component {
 
@@ -9,6 +11,7 @@ class Entreprenuer extends Component {
       <div>
         <h1>This is the Entreprenuer Part</h1>
         <img src="" alt=""/>
+        <ImageSlick/>
         <p>And not just so you can say “Yarr”! (Although that’s really fun, isn’t it? Go ahead. Say it one more time. Yarr!) Being a pirate here means looking around at the work other people are doing and using it as inspiration for your own portfolio.
         And not just so you can say “Yarr”! (Although that’s really fun, isn’t it? Go ahead. Say it one more time. Yarr!) Being a pirate here means looking around at the work other people are doing and using it as inspiration for your own portfolio.
         And not just so you can say “Yarr”! (Although that’s really fun, isn’t it? Go ahead. Say it one more time. Yarr!) Being a pirate here means looking around at the work other people are doing and using it as inspiration for your own portfolio.
