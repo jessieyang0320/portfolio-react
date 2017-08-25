246bc35bdd1ca990869289517e7eeb8c4fc3deab
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-08-25 17:10:49 -0400

    new styling to project page

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -9,6 +9,7 @@ import './Project.css';
 import { Background } from '../../theme/Background.style';
 import Footer from '../../components/Footer/Footer';
 import { Modal } from 'antd';
+import projects from './projects.json';
 
 
 
@@ -19,15 +20,16 @@ class Projects extends Component {
     super()
     this.state={
       visible:false,
-      key:''
+      id:''
     }
   }
 
   showModal = (e)=>{
-    console.log(e.target)
+  
     this.setState({
       visible:true,
-      key: this.key
+      id: e.target.id
+      
     })
   }
 
@@ -46,40 +48,99 @@ class Projects extends Component {
 
 
   render() {
+   
     return (
 
-      <div>
+  <div>
         <NavigationBar/>
         <Background/>
+
+
 
       <div className="projects-container">
               <Reveal effect="animated fadeInUp" className="passage-block">
                   <p>
                       I made beautiful and functional website with React, Nodejs, Express, Rails, MongoDB, Postgres.
                       and as a VR enthusiast, I made VR apps too.
-
                   </p>
 
               </Reveal>
 
 
-            <div className = "project-block">
+              <div className="workGroup2" onClick={this.showModal}>
+
+                    <div href="#" target="_blank" className="inline"  >
+                        <div className="yellowBox">
+                            <img id="1" src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                      News Republic<br/>
+                            <div className="boxesInfo">Web Design / Development</div>
+                        </div>
+                    </div>
 
 
-                <div className="project">               
-                    <div className="fly-left animated slideInLeft">
-                        <h1>01</h1>
-                        <h1> <A className="p-title" href="#" target="_blank">News Republic</A></h1>
-                     </div>
-                
-                  
-                    <div className="fly-right animated slideInRight">
-                        <img src = {require('../../assets/news-republic.png')}
-                     alt="project" onClick={this.showModal} key="1"/>
-                    </div>   
+
+                    <a className="inline">
+                        <div className="yellowBox">
+                            <img id="2" src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                      V-Hab<br/>
+                            <div className="boxesInfo">VR / Three.JS</div>
+                        </div>
+                    </a>
 
 
-                    <Modal  key="1" title="Project Info" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel}>  
+
+                    <a className="inline">
+                        <div className="yellowBox">
+                            <img src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                     Web Design<br/>
+                            <div className="boxesInfo">Web Design / Responsive</div>
+                        </div>
+                    </a>
+
+
+                    <div href="#" target="_blank" className="inline"  >
+                        <div className="yellowBox">
+                            <img id="1" src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                      News Republic<br/>
+                            <div className="boxesInfo">Web Design / Development</div>
+                        </div>
+                    </div>
+
+
+                    <div href="#" target="_blank" className="inline"  >
+                        <div className="yellowBox">
+                            <img id="1" src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                      News Republic<br/>
+                            <div className="boxesInfo">Web Design / Development</div>
+                        </div>
+                    </div>
+
+
+                    <div href="#" target="_blank" className="inline"  >
+                        <div className="yellowBox">
+                            <img id="1" src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                      News Republic<br/>
+                            <div className="boxesInfo">Web Design / Development</div>
+                        </div>
+                    </div>
+
+
+                    <div href="#" target="_blank" className="inline"  >
+                        <div className="yellowBox">
+                            <img id="1" src={require('../../assets/p-icon.png')} alt="news" className="grow"/><br/>
+                                      News Republic<br/>
+                            <div className="boxesInfo">Web Design / Development</div>
+                        </div>
+                    </div>
+
+
+
+                   
+              </div>
+
+
+              <Modal title="Project Info" visible={this.state.id ==="1" && this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} okText="ok"
+          cancelText="cancel">  
                         <div id='inline_content'  visible={this.state.visible}>
                               <div className="popUpLeft">
                                 <img src={require('../../assets/projects/example.jpg')} alt="" />
@@ -96,75 +157,27 @@ class Projects extends Component {
                               </div>
                               <div className="space"></div>
                           </div>
-                    </Modal>  
+              </Modal>  
 
-                    <div className="clear-fix"></div>        
-                </div>
-
+               <div className="clear-fix"></div>     
 
 
-                <div className="project">               
-                    <div className="fly-left animated slideInLeft">
-                        <h1>02</h1>
-                        <h1> <A className="p-title" href="#" target="_blank">V-Hab</A></h1>
-                     </div>
-                
-                  
-                    <div className="fly-right animated slideInRight">
-                        <img src = {require('../../assets/v-hab.png')}
-                     alt="project" onClick={this.showModal}/>
-                    </div>   
-
-
-                    <Modal title="Project Info" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel}>  
-                        <div id='inline_content'  visible={this.state.visible}>
-                              <div className="popUpLeft">
-                                <img src={require('../../assets/projects/example.jpg')} alt="" />
-                              </div>
-                              <div className="popUpRight">
-                                <div className="popUpTitle">V-Hab</div>
-                                    <div className="popUpTech">ThreeJS</div>
-                                    <div className="popUpTech">VR</div>
-                                    <div className="popUpTech">Web Desgin</div>
-                                    <div className="popUpTech">API</div>
-                                  News Republic is a news feed website made with Nodejs and ReactJS                  
-                                  <br/><br/>
-                                <a href="http://briabby.com/" target="_blank"><div className="popUpBtn">LAUNCH WEBSITE</div></a>
-                              </div>
-                              <div className="space"></div>
-                          </div>
-                    </Modal>  
-                    <div className="clear-fix"></div>        
-                </div>
-
-
-
-
-
+            
 
       </div>
 
+      <div className="clearfix"></div>
 
 
-       
 
 
-
-      
-
-
-     
-
-      
-
- 
         <div className = "view-more">
           <A href="https://github.com/jessieyang0320" target="_blank">View More on Github</A>
         </div>
 
-      </div>
+     
       <Footer/>
-      </div>
+</div>
     );
   }
 }
